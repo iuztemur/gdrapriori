e45726216c88f55c7156c7d530f2ee6fa34f4260
kjrz <kuba.jarz@gmail.com>
2014-04-21 15:13:00 +0200

    conditional and decision values encoder

---
+++ src/edami/gdrapriori/data/Labels.java
@@ -0,0 +1,69 @@
+package edami.gdrapriori.data;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Encodes conditional and decision values to integers.
+ *
+ * @author kjrz
+ */
+public class Labels {
+
+    private final Map<String, Integer> conditionalValues = new TreeMap<String, Integer>();
+    private final Map<String, Integer> decisionValues = new TreeMap<String, Integer>();
+
+    // in case reverse translation comes in necessary,
+    // use either commons-collections' BidiMap or guava's BiMap
+
+    public Labels(String[] labels) {
+        readConditionalValues(labels);
+    }
+
+    private void readConditionalValues(String[] labels) {
+        int code = 1;
+
+        for (String label : labels) {
+            // last string are decision classes separated by "/"
+            if (label.contains("/")) {
+                readDecisionValues(label);
+                break;
+            }
+            // all previous strings get int value assigned
+            conditionalValues.put(label, code);
+            code++;
+        }
+    }
+
+    private void readDecisionValues(String label) {
+        int code = 1;
+
+        for (String decisionValue : label.split("/")) {
+            decisionValues.put(decisionValue, code);
+            code++;
+        }
+    }
+
+    public int encodeConditionalValue(String key) {
+        Integer value = conditionalValues.get(key);
+
+        if (value == null) {
+            throw new IllegalArgumentException(
+                    "Key \"" + key + "\" not found in conditional values.");
+        }
+
+        return value;
+    }
+
+
+    public int encodeDecisionValue(String key) {
+        Integer value = decisionValues.get(key);
+
+        if (value == null) {
+            throw new IllegalArgumentException(
+                    "Key \"" + key + "\" not found in decision values.");
+        }
+
+        return value;
+    }
+}
