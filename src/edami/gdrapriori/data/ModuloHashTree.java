7b6802aabecd347158c899df7542aaf6ccc55239
kjrz <kuba.jarz@gmail.com>
2014-05-13 22:56:11 +0200

    initial Hash Tree client implementation

--- src/edami/gdrapriori/data/ModuloHashTree.java
+++ src/edami/gdrapriori/data/ModuloHashTree.java
@@ -1,7 +1,38 @@
 package edami.gdrapriori.data;
 
 /**
- * Created by kjrz on 5/10/14.
+ * @author kjrz
  */
-public class ModuloHashTree {
+public class ModuloHashTree extends TransactionsHashTree {
+
+    private static final int MAX_LEAF_SIZE = 3;
+    // TODO: this one is for mod 2,
+    // TODO: it has to be adjusted otherwise the split won't work
+
+    private final int divisor;
+
+    public ModuloHashTree(Labels labels) {
+        divisor = (int) Math.sqrt((double) labels.conditionalValueMax());
+    }
+
+    @Override
+    public int[] hashFunction(Transaction t) {
+        int[] conditionalValues = t.getConditionalValues();
+        int[] hash = new int[conditionalValues.length];
+
+        for (int i = 0; i < conditionalValues.length; i++) {
+            hash[i] = hashFunction(conditionalValues[i]);
+        }
+
+        return hash;
+    }
+
+    private int hashFunction(int c) {
+        return c % divisor;
+    }
+
+    @Override
+    public int maxLeafSize() {
+        return MAX_LEAF_SIZE;
+    }
 }
--- src/edami/gdrapriori/data/TransactionsHashTree.java
+++ src/edami/gdrapriori/data/TransactionsHashTree.java
@@ -1,9 +1,76 @@
 package edami.gdrapriori.data;
 
+import org.apache.jorphan.collections.HashTree;
+
+import java.util.Arrays;
+
 /**
- *
- *
  * @author kjrz
  */
-public class TransactionsHashTree {
+public abstract class TransactionsHashTree {
+
+    private final HashTree tree = new HashTree();
+
+    public void add(Transaction t) {
+        if (t.getConditionalValues().length == 0) {
+            return; // not of interest
+        }
+
+        int[] hash = hashFunction(t);
+
+        if (tree.size() == 0) {
+            tree.add(Arrays.asList(hash[0]), t);
+            return;
+        }
+
+        HashTree subtree = targetSubtree(hash);
+
+        subtree.add(t);
+
+        if (subtree.size() > maxLeafSize()) {
+            split(subtree);
+        }
+    }
+
+    private HashTree targetSubtree(int[] hash) {
+        HashTree subtree = tree.get(hash[0]);
+
+        int pathLength = 0;
+
+        while (subtree != null && !isLeaf(subtree)) {
+            subtree = subtree.get(hash[++pathLength]);
+        }
+
+        return subtree;
+    }
+
+    private boolean isLeaf(HashTree subtree) {
+        for (Object key : subtree.list()) {
+            if (key instanceof Integer)
+                return false;
+        }
+
+        return true;
+    }
+
+    private void split(HashTree subtree) {
+        HashTree toSplit = (HashTree) subtree.clone();
+        subtree.clear();
+
+        for (Object key : toSplit.list()) {
+            int[] hash = hashFunction((Transaction) key);
+
+            if (hash.length < 2) {
+                subtree.add(Arrays.asList(hash[0]), key);
+            } else {
+                subtree.add(Arrays.asList(hash[0], hash[1]), key);
+            }
+        }
+
+        // TODO: split recursively in case any new leaf > maxLeafSize
+    }
+
+    public abstract int[] hashFunction(Transaction t);
+
+    public abstract int maxLeafSize();
 }
